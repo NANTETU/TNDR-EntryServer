0b133e604b20d3ea25b3a532d00f47e9be175729
なんてつ <189513202+NANTETU@users.noreply.github.com>
2025-04-04 17:50:48 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZs9FyqJ4wjSi-HcVC9SzLentqu8NXBwk",
+  authDomain: "tndr-entryserver.firebaseapp.com",
+  projectId: "tndr-entryserver",
+  storageBucket: "tndr-entryserver.firebasestorage.app",
+  messagingSenderId: "1008351258193",
+  appId: "1:1008351258193:web:55257ec8e18d22b80b3a70",
+};
